94cfea21d00024ea56147dc91b64f280a8d9026d
ctm153 <ctm153@scarletmail.rutgers.edu>
2026-02-28 18:21:51 -0500

    Submit

---
+++ Assets/HighScore.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+    static private Text _uiText;
+    static private int _score = 1000;
+
+    [Tooltip("Check this box to reset the HighScore in PlayerPrefs")]
+    public bool resetHighScoreNow = false;
+
+    void Awake() {
+        _uiText = GetComponent<Text>();
+        if (PlayerPrefs.HasKey("HighScore")) {
+            Score = PlayerPrefs.GetInt("HighScore");
+        }
+        PlayerPrefs.SetInt("HighScore", Score);
+    }
+
+    static public int Score {
+        get { return _score; }
+        private set {
+            _score = value;
+            PlayerPrefs.SetInt("HighScore", value);
+            if (_uiText != null) {
+                _uiText.text = "High Score: " + value.ToString("#,0");
+            }
+        }
+    }
+
+    static public void TRY_SET_HIGH_SCORE(int scoreToTry) {
+        if (scoreToTry <= Score) return;
+        Score = scoreToTry;
+    }
+
+void OnDrawGizmos() {
+    if (resetHighScoreNow) {
+        resetHighScoreNow = false;
+        PlayerPrefs.SetInt("HighScore", 1000);
+        if (_uiText != null) {
+            _uiText.text = "High Score: 1,000";
+        }
+        Debug.LogWarning("PlayerPrefs HighScore reset to 1,000");
+        }
+    }
+}
